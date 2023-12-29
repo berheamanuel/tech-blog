5fd2526a6e0f791c00100ec7cc4e635178fe205f
Amanuel <amanueleman@gmail.com>
2023-12-28 22:00:09 -0500

    connecting database

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+
+// Import Sequelize
+const Sequelize = require('sequelize');
+
+// Import dotenv
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+// export Sequelize
+module.exports = sequelize;
