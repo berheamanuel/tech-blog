f41ed07942d750081d0662112c8669a3e6860068
Amanuel <amanueleman@gmail.com>
2024-01-14 22:03:34 -0500

    Changing user requirement and adding helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,21 @@
 module.exports = {
-//   format_date: (date) => {
-//     // Format date as MM/DD/YYYY
-//     return date.toLocaleDateString();
-//   },
+  format_date: (date) => {
+    // Format date as MM/DD/YYYY
+    return date.toLocaleDateString();
+  },
 
   format_date: (date) => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   },
 
+  format_amount: (amount) => {
+    // format large numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
+
+  is_my_page: (pageUser, userId) => {
+    return pageUser === userId;
+  },
  
 };
 
