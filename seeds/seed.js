678af6e054181bff86ef7ee7a087e81723e6d9ef
Amanuel <amanueleman@gmail.com>
2024-03-16 00:31:31 -0400

    Added try catch to seedDatabase function

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,23 +6,27 @@ const postData = require("./postData.json");
 const commentData = require("./commentData.json");
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  try {
+    await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
     });
+
+    for (const post of postData) {
+      await Post.create({
+        ...post,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
+    }
+
+    await Comment.bulkCreate(commentData);
+
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
   }
-
-  const comments = await Comment.bulkCreate(commentData);
-
-  process.exit(0);
 };
 
 seedDatabase();
